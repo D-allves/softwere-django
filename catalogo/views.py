7693e0ccc9b4227b07eab62010df0d71a4720826
D-allves <danilo7899cepi@gmail.com>
2026-09-17 19:52:36 -0300

    Melhora visual e adiciona filtros ao catalogo

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,11 +1,40 @@
 from django.shortcuts import render, redirect
 from .models import Jogo
+from django.db.models import Avg
 
 
 def lista_jogos(request):
+    busca = request.GET.get('busca', '')
+    status = request.GET.get('status', '')
+    ordem = request.GET.get('ordem', '')
+
     jogos = Jogo.objects.all()
 
-    return render(request, 'catalogo/lista_jogos.html', {'jogos': jogos})
+    if busca:
+        jogos = jogos.filter(nome__icontains=busca)
+
+    if status:
+        jogos = jogos.filter(status__icontains=status)
+
+    if ordem == 'maior':
+        jogos = jogos.order_by('-nota')
+    elif ordem == 'menor':
+        jogos = jogos.order_by('nota')
+
+    total_jogos = Jogo.objects.count()
+    media = Jogo.objects.aggregate(Avg('nota'))['nota__avg']
+
+    if media is not None:
+        media = round(media, 1)
+
+    return render(request, 'catalogo/lista_jogos.html', {
+        'jogos': jogos,
+        'busca': busca,
+        'status': status,
+        'ordem': ordem,
+        'total_jogos': total_jogos,
+        'media': media
+    })
 
 
 def adicionar_jogo(request):
@@ -28,11 +57,13 @@ def adicionar_jogo(request):
 
     return render(request, 'catalogo/adicionar_jogo.html')
 
+
 def excluir_jogo(request, id):
     jogo = Jogo.objects.get(id=id)
     jogo.delete()
 
     return redirect('lista_jogos')
+
 
 def editar_jogo(request, id):
     jogo = Jogo.objects.get(id=id)
